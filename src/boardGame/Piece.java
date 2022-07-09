37edd9ab0b9593c601e1a67f472acede7eeb35d1
William Noel <noelwilliam2018@hotmail.com>
2022-07-09 13:40:02 -0300

    chess layer and printing the board

---
+++ src/boardGame/Piece.java
@@ -0,0 +1,20 @@
+package boardGame;
+
+import javax.swing.text.Position;
+
+public class Piece {
+	protected Position position;
+	protected Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+}
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,26 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Piece;
+
+public class ChessPiece extends Piece{
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	
+	
+	
+	
+}
